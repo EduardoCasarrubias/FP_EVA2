c34ea25543da541636b9b107d6b05b8b547076f5
EduardoCasarrubias <L25550594@chihuahua2.tecnm.mx>
2025-10-02 22:27:49 -0600

    Add files via upload

---
+++ EVA2_3_SWITCH/src/eva2_3_switch/EVA2_3_SWITCH.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_3_switch;
+
+/**
+ *
+ * @author ecasa
+ */
+public class EVA2_3_SWITCH {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String cade = "lunes";
+        switch (cade){
+            case "lunes":
+                System.out.println(1);
+                break;
+            case "martes":
+                System.out.println(2);
+                break;
+            default:
+                System.out.println("dia no existe");
+    }
+        /*double val =5.5
+        switch(val){
+        
+        }
+        boolean bande = false
+        switch(bande){
+        }*/
+    }
+    
+}
