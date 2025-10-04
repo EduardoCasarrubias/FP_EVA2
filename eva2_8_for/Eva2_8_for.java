85f52e1d64863ceea912e1dc1ee9092047c94406
EduardoCasarrubias <L25550594@chihuahua2.tecnm.mx>
2025-10-04 00:56:42 -0600

    Add files via upload

---
+++ eva2_8_for/Eva2_8_for.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_8_for;
+
+/**
+ *
+ * @author ecasa
+ */
+public class Eva2_8_for {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //e parametros
+        //variable y valor de inicio
+        //hasta donde quieres llegar Condicion --> tiene que ser verdad
+        //cuando es falso el for se detiene
+        //incremento o decremento
+        //sumar o restar 
+        //operadodes: añadir 1 o restar 1
+        //++  --
+        //i++ significa sumarle 1 al valor de la i
+        for(int i = 1; i <= 10; i++){
+            System.out.println("hola mundo!!!!!!!!!!!!");
+            
+        for(int i = 10; i >= 1; i--){
+            System.out.println("hola mundo!!!!!!!!!!!!");
+             
+         }
+    }
+    
+}
