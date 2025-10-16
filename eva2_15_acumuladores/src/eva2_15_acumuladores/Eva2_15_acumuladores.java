1e9c1d7f7354d1c844cf72dc77de5d30a7912f0a
EduardoCasarrubias <L25550594@chihuahua2.tecnm.mx>
2025-10-15 23:49:14 -0600

    Add files via upload

---
+++ eva2_15_acumuladores/src/eva2_15_acumuladores/Eva2_15_acumuladores.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_15_acumuladores;
+
+/**
+ *
+ * @author ecasa
+ */
+public class Eva2_15_acumuladores {
+
+    
+    public static void main(String[] args) {
+        //acomulador
+        // ++, --, +=, -=, /=
+        int cuenta = 0;
+        //asignador --->
+        System.out.println(cuenta);
+        cuenta = cuenta + 100; //acomulador 
+        System.out.println(cuenta);
+        cuenta = cuenta + 1000;
+        System.out.println(cuenta);
+        cuenta = cuenta - 800;
+        System.out.println(cuenta);
+        cuenta += 2000; //cuenta = cuenta + 2000;
+    }
+    
+}
