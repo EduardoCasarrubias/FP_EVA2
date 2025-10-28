d4cd28f25865f93a6f54603fee96f0380d894b90
EduardoCasarrubias <L25550594@chihuahua2.tecnm.mx>
2025-10-27 23:21:11 -0600

    Add files via upload

---
+++ eva2_17_alcance/src/eva2_17_alcance/Eva2_17_alcance.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_17_alcance;
+
+/**
+ *
+ * @author ecasa
+ */
+public class Eva2_17_alcance {
+
+    
+    public static void main(String[] args) {
+        int i = 5;
+        {
+            System.out.println(i);
+            int valor = 100;
+            System.out.println(valor);
+        }
+        System.out.println(valor);
+    }
+    
+}
