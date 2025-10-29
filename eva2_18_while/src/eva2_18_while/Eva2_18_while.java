4916a75e56504635a7519b3690e58d6bd9bd57a4
EduardoCasarrubias <L25550594@chihuahua2.tecnm.mx>
2025-10-28 23:20:43 -0600

    Add files via upload

---
+++ eva2_18_while/src/eva2_18_while/Eva2_18_while.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_18_while;
+
+/**
+ *
+ * @author ecasa
+ */
+public class Eva2_18_while {
+
+    
+    public static void main(String[] args) {
+        int i =1;
+        System.out.println("iclo while----->");
+        while(i <= 10 ){//se repite si la condiciones es verdad 
+            System.out.println("eduardo casarrubias");
+            i++;
+        }
+        //similar un for con un while 
+        System.out.println("ciclo do while----->");
+        i=1;
+        do{
+            System.out.println("eduardo casarrubias");
+            i++;
+        }while(i<=10);
+        
+    }
+    
+}
